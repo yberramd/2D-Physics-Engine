aa347c915ccff681e0f5beceaaa4419c078ca2bd
yberramd <younesberramdane1@gmail.com>
2026-04-19 11:46:31 +0200

    A moving shape :)

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,39 @@
 
 int main()
 {
-	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
-	sf::CircleShape shape( 100.f );
+	//Window
+	sf::RenderWindow window( sf::VideoMode( { 640, 480 } ), "SFML works!");
+	sf::RectangleShape shape( {120.f, 100.f} );
 	shape.setFillColor( sf::Color::Green );
+	shape.setPosition({60.f, 50.f});
 
+
+	//Game loop
 	while ( window.isOpen() )
 	{
+		//Event polling
 		while ( const std::optional event = window.pollEvent() )
 		{
-			if ( event->is<sf::Event::Closed>() )
+			if (event->is<sf::Event::Closed>()) {
 				window.close();
+			}
+			else if (const auto* KeyPressed = event->getIf<sf::Event::KeyPressed>())
+			{
+				if (KeyPressed->scancode == sf::Keyboard::Scancode::Escape)
+					window.close();
+			}
 		}
+		//Update
+		shape.move({0.01f, 0.01f});
 
-		window.clear();
-		window.draw( shape );
-		window.display();
+		//Render
+		window.clear(); // Clear old Frame
+
+		window.draw( shape );// Draw the game
+		
+		window.display(); // Tell app that window is done drawing
 	}
+
+	//End of application
+	return 0;
 }
