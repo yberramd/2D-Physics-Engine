69dd479e81e05bd840dd4202b9682dca6e881c40
yberramd <younesberramdane1@gmail.com>
2026-06-27 20:14:43 +0200

    Added collision between rectangles plus added 2 circles

--- src/main.cpp
+++ src/main.cpp
@@ -15,11 +15,18 @@ struct AABB //Construct for a rectangle
 	sf::Vector2f max;
 };
 
+struct Circle // Construct for a circle
+{
+	float radius;
+	sf::Vector2f position;
+};
+
 //Information about the object
 
 struct OBJECT
 {
-	AABB pos;
+	AABB rectangle;
+	Circle circle;
 	sf::Vector2f acceleration;
 	sf::Vector2f speed;
 	float mass;
@@ -28,12 +35,9 @@ struct OBJECT
 
 int	ft_sat(AABB box1, AABB box2)
 {
-	//std::cout << "Pos Box1 min y :" << box1.min.y << "\n";
-	//std::cout << "Pos Box2 min y :" << box2.min.y << "\n";
-	//if((box1.min.y > box2.max.y) || (box2.min.y > box1.max.y)) return false;
+	if((box1.min.y > box2.max.y) || (box2.min.y > box1.max.y)) return false;
 	if((box1.max.x < box2.min.x) || (box1.min.x > box2.max.x)) return false; 
 
-	std::cout << "TEST\n";
 	return true;
 }
 
@@ -43,6 +47,7 @@ int main()
 	sf::RenderWindow window( sf::VideoMode( { 640, 680 } ), "SFML works!");
 
 	//Shape
+	// RECTANGLES
 	sf::VertexArray rectangle(sf::PrimitiveType::TriangleStrip, 4);
 	//Define the position of the rectangle point
 	rectangle[0].position = sf::Vector2f(0.f, 0.f);
@@ -60,8 +65,23 @@ int main()
 	sf::Transform pos_box1, pos_box2;
 	const float* matrix_b1 = pos_box1.getMatrix();
 	const float* matrix_b2 = pos_box2.getMatrix();
-	OBJECT box1, box2;
+
+	//Creating OBJECTS
+	OBJECT box1, box2, d_c1, d_c2;
 	
+	// Initialise info about circle1
+	d_c1.circle.radius = 25.f;
+	d_c1.circle.position = {0.f, 90.f};
+	
+	// Initialise info about circle2
+	d_c2.circle.radius = 30.f;
+	d_c2.circle.position = {580.f, 90.f};
+
+	// CIRCLES
+	sf::CircleShape circle1(d_c1.circle.radius), circle2(d_c2.circle.radius);
+	circle1.setPosition(d_c1.circle.position);
+	circle2.setPosition(d_c2.circle.position);
+
 	// Initialise info about box1 to test Gravity
 	box1.mass = 45;
 
@@ -93,19 +113,34 @@ int main()
 		/*Inner Update*/
 
 		//Box 1
-		box1.pos.min = {matrix_b1[12], matrix_b1[13] + 40};
-		box1.pos.max = {matrix_b1[12] + 40, matrix_b1[13]};
+		box1.rectangle.min = {matrix_b1[12], matrix_b1[13]};
+		box1.rectangle.max = {matrix_b1[12] + 40, matrix_b1[13] + 40};
 		
-		if ((box1.pos.min.y < 580 && box1.pos.max.x < 640) && !ft_sat(box1.pos, box2.pos)) {
-			pos_box1.translate({0.01f, 0.01f});
+		if ((box1.rectangle.min.y < 640 && box1.rectangle.max.x < 640) && !ft_sat(box1.rectangle, box2.rectangle)) {
+			pos_box1.translate({0.01f, 0.f});
 		}
 
 		//Box 2
-		box2.pos.min = {matrix_b2[12], matrix_b2[13] + 40};
-		box2.pos.max = {matrix_b2[12] + 40, matrix_b2[13]};
+		box2.rectangle.min = {matrix_b2[12], matrix_b2[13]};
+		box2.rectangle.max = {matrix_b2[12] + 40, matrix_b2[13] + 40};
 
-		if (box2.pos.min.y < 580 && box2.pos.max.x > 0) {
-			pos_box2.translate({-0.01f, 0.01f});
+		if ((box2.rectangle.min.y < 640 && box2.rectangle.max.x > 40) && !ft_sat(box1.rectangle, box2.rectangle)){
+			pos_box2.translate({-0.01f, 0.f});
+		}
+		else if ((box2.rectangle.min.y < 640 && box2.rectangle.max.x > 40)) {
+			pos_box2.translate({0.f, 0.01f});
+		}
+
+		//Circle 1
+		d_c1.circle.position = circle1.getPosition();
+		if (d_c1.circle.position.y < 640 && d_c1.circle.position.x < 590){
+			circle1.move({0.015f, 0.f});
+		}
+
+		//Circle 2
+		d_c2.circle.position = circle2.getPosition();
+		if (d_c2.circle.position.y < 640 && d_c2.circle.position.x > 0){
+			circle2.move({-0.015f, 0.f});
 		}
 
 		window.clear(); // Clear old Frame
@@ -113,11 +148,12 @@ int main()
 		// Draw the game
 		window.draw(rectangle, pos_box1);
 		window.draw(rectangle, pos_box2);
+		window.draw(circle1);
+		window.draw(circle2);
+		
 		
 		window.display(); // Tell app that window is done drawing
 	}
 	//End of application
 	return 0;
 }
-
-/*Render*/
